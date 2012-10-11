525a354eadab2cb1e4b1ba5d41a15f230d445e6e
Jasper <jaspershemilt@virginmedia.co.uk>
2012-10-11 13:40:23 +0100

    Initial Commit

---
+++ CameraOrientation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CameraOrientation
+//
+//  Created by Jasper Shemilt on 11/10/2012.
+//  Copyright (c) 2012 Jasper Shemilt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CameraOrientation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CameraOrientation
+//
+//  Created by Jasper Shemilt on 11/10/2012.
+//  Copyright (c) 2012 Jasper Shemilt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
